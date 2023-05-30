696163544eda5fc15881397ed6a756501d93f08f
kwg9980 <kwg9980@gmail.com>
2023-05-30 23:30:59 +0900

    Test_1

--- webproject/src/main/java/com/semiproject/webproject/WebprojectApplication.java
+++ webproject/src/main/java/com/semiproject/webproject/WebprojectApplication.java
@@ -2,12 +2,18 @@ package com.semiproject.webproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class WebprojectApplication {
-
+	@RequestMapping("/")
+	String home() {
+		return "안녕하세요";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WebprojectApplication.class, args);
 	}
-
 }
